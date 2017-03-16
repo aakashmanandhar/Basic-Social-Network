e5a23eb44a45e171633da05cc55c9bc4ca2d5ba2
Aakash Manandhar <aakashmanandhar@gmail.com>
2017-03-16 23:41:59 +0545

    Social Network V-22

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,11 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
+from accounts.forms import (
+    RegistrationForm,
+    EditProfileFrom,
+)
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserChangeForm, PasswordChangeForm
+from django.contrib.auth import update_session_auth_hash
 
 # Create your views here.
 def home(request):
@@ -6,3 +13,47 @@ def home(request):
     name = 'Aakash Manandhar'
     args = {'MyName': name, 'MyNumbers': numbers}
     return render(request, 'accounts/home.html', args)
+
+def register(request):
+    if request.method =='POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/account')
+    else:
+        form = RegistrationForm()
+
+        args = {'form': form}
+        return render(request, 'accounts/reg_form.html', args)
+
+def view_profile(request):
+    args = {'user': request.user}
+    return render(request, 'accounts/profile.html', args)
+
+def edit_profile(request):
+    if request.method == 'POST':
+        form = EditProfileFrom(request.POST, instance=request.user)
+
+        if form.is_valid():
+            form.save()
+            return redirect('/account/profile')
+    else:
+        form = EditProfileFrom(instance=request.user)
+        args = {'form': form}
+        return render(request, 'accounts/edit_profile.html', args)
+
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(data=request.POST, user=request.user)
+
+        if form.is_valid():
+            form.save()
+            update_session_auth_hash(request, form.user)
+            return redirect('/account/profile')
+        else:
+            return redirect('/account/change-password')
+
+    else:
+        form = PasswordChangeForm(user=request.user)
+        args = {'form': form}
+        return render(request, 'accounts/change_password.html', args)
